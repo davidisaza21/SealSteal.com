4b09d7a59c2b4b96113e68f7b0323aec4fe7761a
David Isaza <Davidisaza21@gmail.com>
2015-12-15 18:03:38 -0500

    updating work to footer and products index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Shoppe::Product.root.ordered.includes(:product_categories, :variants)
     @products = @products.group_by(&:product_category)
-    @search = Search.new(Shoppe::Product, params[:search])
-    @users = @search.run
   end
+
 
   def show
     @product = Shoppe::Product.root.find_by_permalink(params[:permalink])
